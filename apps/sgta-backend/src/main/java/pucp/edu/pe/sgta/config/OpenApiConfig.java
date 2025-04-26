c8b68b0e8ad43d7f8b77a9af0db69c082c23abcd
Diego Ochoa <daochoa@pucp.edu.pe>
2025-04-25 22:27:46 -0500

    chore(backend): agregar swagger open api

---
+++ apps/sgta-backend/src/main/java/pucp/edu/pe/sgta/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package pucp.edu.pe.sgta.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+
+@Configuration
+public class OpenApiConfig {
+    
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("SGTA API - Sistema de Gestión de Tesis PUCP")
+                        .description("Documentación OpenAPI para el backend del sistema SGTA.")
+                        .version("v1.0"));
+    }
+}
