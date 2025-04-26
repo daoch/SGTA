1cbdaa20597015cd355183ebe6a697756ff6fdf9
Diego Ochoa <daochoa@pucp.edu.pe>
2025-04-25 23:39:47 -0500

    chore(backend): se elimino la libreria de dotenv ya que springboot ya lo maneja de forma nativa

--- apps/sgta-backend/src/main/java/pucp/edu/pe/sgta/SgtaApplication.java
+++ apps/sgta-backend/src/main/java/pucp/edu/pe/sgta/SgtaApplication.java
@@ -1,6 +1,5 @@
 package pucp.edu.pe.sgta;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,19 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SgtaApplication {
 
 	public static void main(String[] args) {
-
-		//ESTO SE USARA CUANDO SE TENGA LA BASE DE DATOS
-		/*Dotenv dotenv = Dotenv.load();
-
-		//Obetener las variables de entorno
-		String dbUrl = dotenv.get("DB_URL");
-		String dbUser = dotenv.get("DB_USER");
-		String dbPass = dotenv.get("DB_PASS");
-
-		//Establecer las variables en el sistemas
-		System.setProperty("DB_URL", dbUrl);
-		System.setProperty("DB_USER", dbUser);
-		System.setProperty("DB_PASS", dbPass);*/
 		SpringApplication.run(SgtaApplication.class, args);
 	}
 
