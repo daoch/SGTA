49a42818f1ad81413a65fb4e9c41c7f10189f412
Renzo Iwamoto <130329342+renzoIwamoto@users.noreply.github.com>
2025-05-01 22:30:33 -0500

    fix: tipo de dato de fecha_limite del model tema corregido

--- apps/sgta-backend/src/main/java/pucp/edu/pe/sgta/dto/TemaDto.java
+++ apps/sgta-backend/src/main/java/pucp/edu/pe/sgta/dto/TemaDto.java
@@ -28,7 +28,7 @@ public class TemaDto {
     private String metodologia;
     private String portafolioUrl;
     private boolean activo;
-    private ooffsetDateTime fechaLimite;
+    private OffsetDateTime fechaLimite;
     private OffsetDateTime fechaCreacion;
     private OffsetDateTime fechaModificacion;
     private List<Integer> idUsuarioInvolucradosList;
