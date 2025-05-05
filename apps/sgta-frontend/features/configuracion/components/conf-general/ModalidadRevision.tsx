496fc4c3f7b63a3175fae346d27d0313937c7985
Alexandra Zavala <alexandra.zavala@pucp.edu.pe>
2025-05-04 23:17:36 -0500

    feat(configuracion): add comments for test

--- apps/sgta-frontend/features/configuracion/components/conf-general/ModalidadRevision.tsx
+++ apps/sgta-frontend/features/configuracion/components/conf-general/ModalidadRevision.tsx
@@ -7,11 +7,33 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { useState } from "react";
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
+//import { updateCarreraXParametroConfiguracion } from "../../services/configuracion-service";
 
 export default function ModalidadRevisionCard() {
-  
+  // const [turnitinEnabled, setTurnitinEnabled] = useState(true);
+  // const [antiplagioEnabled, setAntiplagioEnabled] = useState(true);
+
+  // const handleToggle = async (parametro: string, value: boolean) => {
+  //   try {
+  //     await updateCarreraXParametroConfiguracion({
+  //       id: parametro === "turnitin" ? 1 : 2, 
+  //       valor: value.toString(),
+  //       carreraId: 1, 
+  //       parametroConfiguracionId: parametro === "turnitin" ? 1 : 2, 
+  //     });
+  //     if (parametro === "turnitin") {
+  //       setTurnitinEnabled(value);
+  //     } else {
+  //       setAntiplagioEnabled(value);
+  //     }
+  //   } catch (error) {
+  //     console.error("Error al actualizar el parámetro:", error);
+  //   }
+  // };
+
   return (
     <Card>
       <CardHeader>
@@ -25,14 +47,15 @@ export default function ModalidadRevisionCard() {
           <Label htmlFor="habilitar-turnitin">
             Habilitar subida de informes Turnitin
           </Label>
-          <Switch id="habilitar-turnitin" defaultChecked />
+          <Switch id="habilitar-turnitin" />
         </div>
 
         <div className="flex items-center justify-between space-x-2">
           <Label htmlFor="integracion-antiplagio">
             Activar integración con sistemas antiplagio
           </Label>
-          <Switch id="integracion-antiplagio" />
+          <Switch id="integracion-antiplagio"
+          />
         </div>
       </CardContent>
     </Card>
---
+++ apps/sgta-frontend/features/configuracion/types/configuracion.types.ts
@@ -0,0 +1,14 @@
+// export type Parametro = {
+//     id: string;
+//     nombre: string;
+//     valor: string;
+// };
+
+// export type ParametrosState = {
+//     parametros: Parametro[];
+//     isLoading: boolean;
+//     error: string | null;
+//     fetchParametros: () => Promise<void>;
+//     updateParametro: (id: string, nuevoValor: string) => void; // Función para actualizar el valor del parámetro
+//     clearError: () => void;
+// };
