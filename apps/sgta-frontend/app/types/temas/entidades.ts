d1548b258ec052f5782f7f31ee3f1c670af6ae7f
Luis Mesajil <lhmesajil@gmail.com>
2025-05-06 18:17:53 -0500

    fea(entidades): define Tema interface with detailed properties for temas management

--- apps/sgta-frontend/app/types/temas/entidades.ts
+++ apps/sgta-frontend/app/types/temas/entidades.ts
@@ -9,3 +9,19 @@ export interface TemaForm {
   requisitos?: string;
   fechaLimite?: string;
 }
+
+export interface Tema {
+  id: number;
+  titulo: string;
+  area: string;
+  asesor: string;
+  estudiantes: { nombre: string; codigo: string }[] | null;
+  postulaciones: number | null;
+  estado: string;
+  tipo: string;
+  ciclo: string;
+  descripcion: string;
+  coasesores: string[];
+  recursos: { nombre: string; tipo: string; fecha: string }[];
+}
+
