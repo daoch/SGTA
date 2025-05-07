3007319beef87d954a1526ffb04b3a2771f21b8c
ls-falcon <142854534+ls-falcon@users.noreply.github.com>
2025-05-07 15:28:19 -0500

    .

--- apps/sgta-backend/src/main/java/pucp/edu/pe/sgta/dto/EtapaFormativaXCicloDto.java
+++ apps/sgta-backend/src/main/java/pucp/edu/pe/sgta/dto/EtapaFormativaXCicloDto.java
@@ -1,12 +1,29 @@
 package pucp.edu.pe.sgta.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class EtapaFormativaXCicloDto {
+public class EtapaFormativaXCicloDTO {
+
     private Integer id;
+
+    private Integer etapaFormativaId;
+
+    private Integer cicloId;
+
+    private boolean activo;
+
+    private OffsetDateTime fechaCreacion;
+
+    private OffsetDateTime fechaModificacion;
 }
