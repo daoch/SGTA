a5d1582b241962f551d27365043157f09dbe4718
luciaDC <lucia.delcastillom@pucp.edu.pe>
2025-05-08 14:59:41 -0500

    Cambios para limite de tesistas

--- apps/sgta-frontend/features/configuracion/components/conf-general/Asesores.tsx
+++ apps/sgta-frontend/features/configuracion/components/conf-general/Asesores.tsx
@@ -8,7 +8,6 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
 import { Switch } from "@/components/ui/switch";
 
 export default function AsesoresCards() {
