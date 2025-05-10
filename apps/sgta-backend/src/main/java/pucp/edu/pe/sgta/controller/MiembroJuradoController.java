a2859032021399fd5a5d469f86cda9352d3b7cb2
MichaelGamarraH <a20196119@pucp.edu.pe>
2025-05-10 11:03:05 -0500

    Añadido MiembrosJuradosCoordinador

---
+++ apps/sgta-backend/src/main/java/pucp/edu/pe/sgta/controller/MiembroJuradoController.java
@@ -0,0 +1,40 @@
+package pucp.edu.pe.sgta.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pucp.edu.pe.sgta.dto.MiembroJuradoDto;
+import pucp.edu.pe.sgta.service.inter.AutorService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/jurado")
+public class MiembroJuradoController {
+
+
+    private final AutorService autorService;
+
+    @Autowired
+    public MiembroJuradoController(AutorService autorService) {
+        this.autorService = autorService;
+    }
+
+
+    @GetMapping
+    public List<MiembroJuradoDto> obtenerUsuarioTemaInfo() {
+        return autorService.obtenerUsuarioTemaInfo();
+    }
+    @GetMapping("/estado/{estado}")
+    public List<MiembroJuradoDto> obtenerUsuariosPorEstado(@PathVariable Boolean estado) {
+        return autorService.obtenerUsuariosPorEstado(estado);
+    }
+    @GetMapping("/area/{areaConocimientoId}")
+    public List<MiembroJuradoDto> obtenerUsuariosPorAreaConocimiento(
+            @PathVariable Integer areaConocimientoId) {
+        return autorService.obtenerUsuariosPorAreaConocimiento(areaConocimientoId);
+    }
+
+}
