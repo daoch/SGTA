4526e9ef27be29d209f487846239a6bcccc6774c
Diego Ochoa <daochoa@pucp.edu.pe>
2025-05-14 10:03:17 -0500

    Add: Backend & Frontend security config to admit bearer token

--- apps/sgta-backend/src/main/java/pucp/edu/pe/sgta/security/SecurityConfig.java
+++ apps/sgta-backend/src/main/java/pucp/edu/pe/sgta/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package pucp.edu.pe.sgta.security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -25,6 +26,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+            .cors(Customizer.withDefaults())
             .csrf(csrf -> csrf.disable())
             .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
