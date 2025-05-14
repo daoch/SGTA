c1f85aca835ddd9565255c99ebccc9dcbdae789c
Alberto Renato Castillo Alarcón <a20193439@pucp.edu.pe>
2025-05-14 03:01:25 -0500

    function list carrera
    lista todas las carreras

---
+++ apps/sgta-backend/src/main/java/pucp/edu/pe/sgta/service/inter/CarreraService.java
@@ -0,0 +1,16 @@
+package pucp.edu.pe.sgta.service.inter;
+
+import pucp.edu.pe.sgta.dto.CarreraDto;
+
+import java.util.List;
+
+public interface CarreraService {
+
+    CarreraDto findById(Integer id);
+    
+    List<CarreraDto> getAll();
+    
+    List<CarreraDto> getAllActive();
+    
+    List<CarreraDto> getCarrerasByUsuario(Integer usuarioId);
+} 
