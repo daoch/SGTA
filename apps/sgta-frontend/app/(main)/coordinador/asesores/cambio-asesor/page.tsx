6010ebf689f5e4d3244d4d660a4a8e8fe937439a
yPerez95 <144288965+yPerez95@users.noreply.github.com>
2025-05-15 15:22:32 -0500

    mend
    Fix Api integration

--- apps/sgta-frontend/app/(main)/coordinador/asesores/cambio-asesor/page.tsx
+++ apps/sgta-frontend/app/(main)/coordinador/asesores/cambio-asesor/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import ListaSolicitudesCambioAsesor from "@/features/asesores/views/list-assessor-change-request";
+import ListaSolicitudesCambioAsesor from "@/features/asesores/views/view-coordinator-assessor-change-requests";
 
 const Page: React.FC = () => {
   return <ListaSolicitudesCambioAsesor />;
--- apps/sgta-frontend/app/(main)/dashboard/page.tsx
+++ apps/sgta-frontend/app/(main)/dashboard/page.tsx
@@ -1,10 +1,9 @@
 "use client";
+
 import React from "react";
-import ListaSolicitudesCambioAsesor from "@/features/asesores/views/view-coordinator-assessor-change-requests";
-import ListaSolicitudesCeseAsesoria from "@/features/asesores/views/view-coordinator-cessation-requests.tsx";
 
 const Page: React.FC = () => {
-  return <ListaSolicitudesCambioAsesor />;
+  return <div>page</div>;
 };
 
 export default Page;
