9675e18a1bc2ec451bc804c9807421812331a7b7
nicoleiparragz <iparraguirre.nicole@pucp.edu.pe>
2025-05-20 19:27:23 -0500

    listado de entregables por ciclo y tema

--- apps/sgta-backend/src/main/java/pucp/edu/pe/sgta/service/inter/EntregableService.java
+++ apps/sgta-backend/src/main/java/pucp/edu/pe/sgta/service/inter/EntregableService.java
@@ -1,11 +1,13 @@
 package pucp.edu.pe.sgta.service.inter;
 
 import pucp.edu.pe.sgta.dto.EntregableDto;
+import pucp.edu.pe.sgta.dto.EntregableXTemaDto;
 
 import java.util.List;
 
 public interface EntregableService {
     List<EntregableDto> listarEntregablesXEtapaFormativaXCiclo(Integer etapaFormativaXCicloId);
+    List<EntregableXTemaDto> listarEntregablesConEnvioXEtapaFormativaXCiclo(Integer etapaFormativaXCicloId, Integer temaId);
     Integer create(Integer etapaFormativaXCicloId, EntregableDto entregableDto);
     List<EntregableDto> getAll(); // Obtiene la lista de entregables
     void update(EntregableDto entregableDto); // Actualiza un entregable
