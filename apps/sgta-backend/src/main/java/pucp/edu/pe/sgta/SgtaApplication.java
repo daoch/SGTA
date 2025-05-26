41bb69964e52e948363785e651edbfd37ec66b18
luciaDC <lucia.delcastillom@pucp.edu.pe>
2025-05-26 16:45:22 -0500

    Comentando el schema

--- apps/sgta-backend/src/main/java/pucp/edu/pe/sgta/SgtaApplication.java
+++ apps/sgta-backend/src/main/java/pucp/edu/pe/sgta/SgtaApplication.java
@@ -18,7 +18,7 @@ public class SgtaApplication {
 		System.setProperty("DB_URL", dotenv.get("DB_URL", ""));
 		System.setProperty("DB_USER", dotenv.get("DB_USER", ""));
 		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD", ""));
-		System.setProperty("DB_SCHEMA", dotenv.get("DB_SCHEMA", ""));
+		//System.setProperty("DB_SCHEMA", dotenv.get("DB_SCHEMA", ""));
 		System.setProperty("CORS_ALLOWED_ORIGIN", dotenv.get("CORS_ALLOWED_ORIGIN", ""));
 		System.setProperty("AWS_S3_BUCKET", dotenv.get("AWS_S3_BUCKET", ""));
 		System.setProperty("AWS_ACCESS_KEY_ID", dotenv.get("AWS_ACCESS_KEY_ID", ""));
