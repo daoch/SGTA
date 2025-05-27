8c643120f250d6880f7b2934a28d56cf089decc2
darkoso7 <96436492+darkoso7@users.noreply.github.com>
2025-05-26 19:05:50 -0500

    Renombrar de nuevo a RolDto.java

--- apps/sgta-backend/src/main/java/pucp/edu/pe/sgta/dto/RolDto.java
+++ apps/sgta-backend/src/main/java/pucp/edu/pe/sgta/dto/RolDto.java
@@ -1,10 +1,7 @@
 package pucp.edu.pe.sgta.dto;
 
-import java.time.OffsetDateTime;
-
 import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,13 +10,8 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
-public class RolDto {
+public class RolDTO {
 
     private Integer id;
     private String nombre;
-    private String descripcion;
-    private Boolean activo;
-    private OffsetDateTime fechaCreacion;
-    private OffsetDateTime fechaModificacion;
 }
