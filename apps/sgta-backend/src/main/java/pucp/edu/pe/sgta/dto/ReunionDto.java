d55a6c1e1f35567888b2bdb44bb4b67eecaef8f1
Branny Pena <pena.branny@pucp.edu.pe>
2025-05-26 22:47:46 -0500

    controller, dtos y mappers para usuario y usuarioxreunion

---
+++ apps/sgta-backend/src/main/java/pucp/edu/pe/sgta/dto/ReunionDto.java
@@ -0,0 +1,27 @@
+package pucp.edu.pe.sgta.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReunionDto {
+
+    private Integer id;
+    private String titulo;
+    private OffsetDateTime fechaHoraInicio;
+    private OffsetDateTime fechaHoraFin;
+    private String descripcion;
+    private Boolean disponible;
+    private String url;
+    private Boolean activo;
+    private OffsetDateTime fechaCreacion;
+    private OffsetDateTime fechaModificacion;
+
+}
