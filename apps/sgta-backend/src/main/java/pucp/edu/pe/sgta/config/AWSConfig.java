9657150828708ff79bf6653eebc593d0fcb84b5f
ls-falcon <142854534+ls-falcon@users.noreply.github.com>
2025-05-28 01:11:16 -0500

    feat(gestion):Subida de entregables a s3 Backend

--- apps/sgta-backend/src/main/java/pucp/edu/pe/sgta/config/AWSConfig.java
+++ apps/sgta-backend/src/main/java/pucp/edu/pe/sgta/config/AWSConfig.java
@@ -2,20 +2,26 @@ package pucp.edu.pe.sgta.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class AWSConfig {
-    @Value("${AWS_REGION}")
-    private String awsRegion;
+
+    private final String awsRegion = System.getProperty("AWS_REGION");
+    private final String awsAccessKeyId = System.getProperty("AWS_ACCESS_KEY_ID");
+    private final String awsSecretAccessKey = System.getProperty("AWS_SECRET_ACCESS_KEY");
+
+    AwsBasicCredentials awsCreds = AwsBasicCredentials.create(awsAccessKeyId, awsSecretAccessKey);
 
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
                 .region(Region.of(awsRegion))
-                .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
                 .build();
     }
 }
