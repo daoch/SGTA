2f97e67664030322a370dcbba0c819edd032b95c
nicoleiparragz <iparraguirre.nicole@pucp.edu.pe>
2025-05-29 03:49:00 -0500

    fix: completar imports

--- apps/sgta-backend/src/main/java/pucp/edu/pe/sgta/controller/UsuarioController.java
+++ apps/sgta-backend/src/main/java/pucp/edu/pe/sgta/controller/UsuarioController.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import pucp.edu.pe.sgta.dto.asesores.FiltrosDirectorioAsesores;
 import pucp.edu.pe.sgta.dto.asesores.PerfilAsesorDto;
 import pucp.edu.pe.sgta.dto.asesores.UsuarioConRolDto;
 import pucp.edu.pe.sgta.dto.asesores.UsuarioFotoDto;
