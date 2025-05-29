e0dbd951be78fe9998a9765acba6ee592489081b
Sebastian Cerna Challe <sebastian.cerna@pucp.edu.pe>
2025-05-29 02:35:56 -0500

    fix_s3_mock

--- apps/sgta-backend/src/test/java/pucp/edu/pe/sgta/SgtaApplicationTests.java
+++ apps/sgta-backend/src/test/java/pucp/edu/pe/sgta/SgtaApplicationTests.java
@@ -2,14 +2,21 @@ package pucp.edu.pe.sgta;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.Mock;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
+import software.amazon.awssdk.services.s3.S3Client;
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class SgtaApplicationTests {
+	@Mock
+	private S3Client s3Client;
 
 	@Test
 	void contextLoads() {
 	}
-
 }
+
+
