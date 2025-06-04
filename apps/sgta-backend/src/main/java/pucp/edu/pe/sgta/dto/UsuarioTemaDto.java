d70eeea948ce0e0175e1ac39abe31e857585b28c
renzoIwamoto <renzo.iwamoto@pucp.edu.pe>
2025-06-03 20:49:51 -0500

    fix: listar_area eliminado duplicado

---
+++ apps/sgta-backend/src/main/java/pucp/edu/pe/sgta/dto/UsuarioTemaDto.java
@@ -0,0 +1,16 @@
+package pucp.edu.pe.sgta.dto;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UsuarioTemaDto {
+    private Integer id;
+    private Integer usuarioId;
+    private Integer temaId;
+    private Boolean activo;
+}
