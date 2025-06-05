1a43819e901efaed2fdad90c14905b1a2ce58386
Diego Ochoa <daochoa@pucp.edu.pe>
2025-06-04 22:21:41 -0500

    Add: estado de usuario en exposicion

--- apps/sgta-frontend/features/jurado/components/PlanificationComponents/tooltip-bloque.tsx
+++ apps/sgta-frontend/features/jurado/components/PlanificationComponents/tooltip-bloque.tsx
@@ -1,51 +1,62 @@
 import { Tema } from "../../types/jurado.types";
 
 interface Props {
-  expoFind: Tema;
+  tema: Tema;
 }
 
-export default function ToolTipoBloque({ expoFind }: Props) {
+export default function ToolTipoBloque({ tema }: Props) {
   return (
     <div className="flex flex-col gap-2 p-2 max-w-60">
       <section>
-        <strong>Estado planificacion: </strong>
-        <div className="flex flex-row ml-2 gap-2 items-center">
+        <strong>Estado tema: </strong>
+        <div className="flex flex-row gap-2 items-center">
           <div className="w-3 h-3 rounded-full bg-green-300" />
-          <span>Programada</span>
+          <span className="text-gray-300">Programada</span>
         </div>
       </section>
 
       <div className="flex flex-col">
         <strong>Codigo: </strong>
-        <span className="ml-2">{expoFind.codigo}</span>
+        <span className="text-gray-300">{tema.codigo}</span>
       </div>
 
       <div className="flex flex-col">
         <strong>Titulo: </strong>
-        <span className="ml-2"> {expoFind.titulo}</span>
+        <span className="text-gray-300"> {tema.titulo}</span>
       </div>
 
       <div className="flex flex-col">
-        <strong>Jurados:</strong>
-        <ul>
-          {expoFind?.usuarios
-            ?.filter((u) => u.rol?.nombre === "Jurado")
-            .map((j) => (
-              <li className="ml-2" key={j.idUsario}>
-                {j.nombres} {j.apellidos}
-              </li>
-            ))}
-        </ul>
-      </div>
-
-      <div className="flex flex-col">
-        <strong>Asesor:</strong>
-        {expoFind?.usuarios
-          ?.filter((u) => u.rol?.nombre === "Asesor")
+        <strong>Miembros de jurado :</strong>
+        {tema?.usuarios
+          ?.filter((u) => u.rol?.nombre !== "Tesista")
           .map((a) => (
-            <span className="ml-2" key={a.idUsario}>
-              {a.nombres} {a.apellidos}
-            </span>
+            <li key={a.idUsario}>
+              <span className="text-gray-300">
+                {a.nombres} {a.apellidos}
+              </span>
+              <div className="ml-5 text-gray-300">
+                {a.estadoRespuesta === "aceptado" && (
+                  <div className="flex flex-row gap-2 items-center">
+                    <div className="w-3 h-3 rounded-full bg-green-500" />
+                    <span className="text-green-500 font-medium">Aceptado</span>
+                  </div>
+                )}
+                {a.estadoRespuesta === "esperando_respuesta" && (
+                  <div className="flex flex-row gap-2 items-center">
+                    <div className="w-3 h-3 rounded-full bg-yellow-500" />
+                    <span className="text-yellow-500 font-medium">
+                      Pendiente
+                    </span>
+                  </div>
+                )}
+                {a.estadoRespuesta === "rechazado" && (
+                  <div className="flex flex-row gap-2 items-center">
+                    <div className="w-3 h-3 rounded-full bg-red-500" />
+                    <span className="text-red-500 font-medium">Rechazado</span>
+                  </div>
+                )}
+              </div>
+            </li>
           ))}
       </div>
     </div>
