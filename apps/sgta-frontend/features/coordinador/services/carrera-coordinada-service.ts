89d69f7a3688355a5b655d07f41ee3ae3d3b9a13
nicoleiparragz <iparraguirre.nicole@pucp.edu.pe>
2025-06-05 04:04:47 -0500

    fix: service de carrera coordinada

---
+++ apps/sgta-frontend/features/coordinador/services/carrera-coordinada-service.ts
@@ -0,0 +1,9 @@
+import axios from "@/lib/axios/axios-instance";
+import { CarreraDto } from "../dtos/CarreraDto";
+
+export const getCarreraCoordinadaPorUsuario = async (usuarioId: number): Promise<CarreraDto> => {
+  const response = await axios.get("/carrera/coordinada-por", {
+    params: { usuarioId },
+  });
+  return response.data;
+};
