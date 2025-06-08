bac95c4f2584412b0cdede662843ea232a7b8c36
xbreadze <a20193541@pucp.edu.pe>
2025-06-08 17:38:18 -0500

    update

--- apps/sgta-backend/src/main/java/pucp/edu/pe/sgta/service/imp/TemaServiceImpl.java
+++ apps/sgta-backend/src/main/java/pucp/edu/pe/sgta/service/imp/TemaServiceImpl.java
@@ -2749,6 +2749,11 @@ public class TemaServiceImpl implements TemaService {
                     UsuarioDto u = new UsuarioDto();
                     u.setId(tesistaIdsArr[i]);
                     u.setNombres(tesistaNombresArr[i]);
+					Optional<UsuarioXTema> ut = usuarioTemaRepository.findByUsuario_IdAndTema_Id(u.getId(),dto.getId());
+					u.setRechazado(ut.get().getRechazado());
+					u.setAsignado(ut.get().getAsignado());
+					u.setActivo(ut.get().getActivo());
+					u.setCreador(ut.get().getCreador());
                     listaTesistas.add(u);
                 }
             }
