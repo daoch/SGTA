a3862b8dbf99c76c6a08943cd202e349f84a6108
nicoleiparragz <iparraguirre.nicole@pucp.edu.pe>
2025-06-10 23:05:31 -0500

    fix: correccion del service getCarreraCoordinadaPorUsuario

--- apps/sgta-frontend/features/coordinador/services/carrera-coordinada-service.ts
+++ apps/sgta-frontend/features/coordinador/services/carrera-coordinada-service.ts
@@ -2,7 +2,7 @@ import axios from "@/lib/axios/axios-instance";
 import { CarreraDto } from "../dtos/CarreraDto";
 
 export const getCarreraCoordinadaPorUsuario = async (usuarioId: number): Promise<CarreraDto> => {
-  const response = await axios.get("/carrera/coordinada-por", {
+  const response = await axios.get("/carrera/coordinada-por-usuario", {
     params: { usuarioId },
   });
   return response.data;
