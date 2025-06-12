7c6b9710de56e64840525eb0b406c86fef0dcde1
Alex <140289224+alexitopro@users.noreply.github.com>
2025-06-12 01:39:28 -0500

    fix(jurado): cambiado == programada para que pase a completada en estado exposiicon job

--- apps/sgta-backend/src/main/java/pucp/edu/pe/sgta/job/EstadoExposicionJob.java
+++ apps/sgta-backend/src/main/java/pucp/edu/pe/sgta/job/EstadoExposicionJob.java
@@ -1,5 +1,6 @@
 package pucp.edu.pe.sgta.job;
 
+import org.antlr.v4.runtime.atn.SemanticContext.AND;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -21,7 +22,6 @@ import java.util.List;
 @Component
 public class EstadoExposicionJob implements Job {
 
-
     private final JornadaExposicionRepository jornadaExposicionRepository;
 
     private final JornadaExposicionXSalaExposicionRepository jornadaXSalaRepository;
@@ -30,13 +30,15 @@ public class EstadoExposicionJob implements Job {
 
     private final ExposicionXTemaRepository exposicionXTemaRepository;
 
-    public EstadoExposicionJob(JornadaExposicionRepository jornadaExposicionRepository, JornadaExposicionXSalaExposicionRepository jornadaXSalaRepository, BloqueHorarioExposicionRepository bloqueHorarioRepository, ExposicionXTemaRepository exposicionXTemaRepository) {
+    public EstadoExposicionJob(JornadaExposicionRepository jornadaExposicionRepository,
+            JornadaExposicionXSalaExposicionRepository jornadaXSalaRepository,
+            BloqueHorarioExposicionRepository bloqueHorarioRepository,
+            ExposicionXTemaRepository exposicionXTemaRepository) {
         this.jornadaExposicionRepository = jornadaExposicionRepository;
         this.jornadaXSalaRepository = jornadaXSalaRepository;
         this.bloqueHorarioRepository = bloqueHorarioRepository;
         this.exposicionXTemaRepository = exposicionXTemaRepository;
     }
-
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
@@ -48,20 +50,20 @@ public class EstadoExposicionJob implements Job {
             OffsetDateTime fin = jornada.getDatetimeFin();
 
             if (fin.isBefore(ahora)) {
-                List<JornadaExposicionXSalaExposicion> salas =
-                        jornadaXSalaRepository.findByJornadaExposicionId(jornada.getId());
+                List<JornadaExposicionXSalaExposicion> salas = jornadaXSalaRepository
+                        .findByJornadaExposicionId(jornada.getId());
 
                 for (JornadaExposicionXSalaExposicion sala : salas) {
-                    List<BloqueHorarioExposicion> bloques =
-                            bloqueHorarioRepository.findByJornadaExposicionXSalaId(Long.valueOf(sala.getId()));
+                    List<BloqueHorarioExposicion> bloques = bloqueHorarioRepository
+                            .findByJornadaExposicionXSalaId(Long.valueOf(sala.getId()));
                     for (BloqueHorarioExposicion bloque : bloques) {
                         ExposicionXTema exposicion = bloque.getExposicionXTema();
 
                         if (exposicion == null) {
                             continue;
                         }
-                        if (exposicion.getEstadoExposicion() != EstadoExposicion.PROGRAMADA) {
-                            exposicion.setEstadoExposicion(EstadoExposicion.PROGRAMADA);
+                        if (exposicion.getEstadoExposicion() == EstadoExposicion.PROGRAMADA) {
+                            exposicion.setEstadoExposicion(EstadoExposicion.COMPLETADA);
                             exposicionXTemaRepository.save(exposicion);
                         }
                     }
