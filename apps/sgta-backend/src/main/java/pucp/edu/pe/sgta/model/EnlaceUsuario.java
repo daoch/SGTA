774efdac7b300953d5939e765cb15f70312d7111
Drew Ames <carlo.ames@pucp.edu.pe>
2025-06-11 19:24:40 -0500

    Agregada tabla enlace_usuario

---
+++ apps/sgta-backend/src/main/java/pucp/edu/pe/sgta/model/EnlaceUsuario.java
@@ -0,0 +1,42 @@
+package pucp.edu.pe.sgta.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "enlace_usuario")
+public class EnlaceUsuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "enlace_usuario_id")
+    private Integer id;
+
+    @Column(name = "plataforma", nullable = false, length = 100)
+    private String plataforma;
+
+    @Column(name = "enlace", nullable = false, length = 250)
+    private String enlace;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "usuario_id", nullable = false, foreignKey = @ForeignKey(name = "fk_enlace_usuario_u"))
+    private Usuario usuario;
+
+    @Column(nullable = false)
+    private Boolean activo = true;
+
+    @Column(name = "fecha_creacion", nullable = false, insertable = false, columnDefinition = "TIMESTAMP WITH TIME ZONE")
+    private OffsetDateTime fechaCreacion;
+
+    @Column(name = "fecha_modificacion", insertable = false, columnDefinition = "TIMESTAMP WITH TIME ZONE")
+    private OffsetDateTime fechaModificacion;
+
+}
